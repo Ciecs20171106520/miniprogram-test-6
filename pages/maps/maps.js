a069a6441d33e93a154279eb9d8fa045f1f10053
Ciecs20171106520 <43473446+Ciecs20171106520@users.noreply.github.com>
2019-06-24 23:03:42 +0800

    123

--- pages/maps/maps.js
+++ pages/maps/maps.js
@@ -11,11 +11,11 @@ Page({
     covers: [{
       latitude: 23.099994,
       longitude: 113.344520,
-      iconPath: '/images/location.png'
+      iconPath: '/pages/images/location.png'
     }, {
       latitude: 23.099994,
       longitude: 113.304520,
-      iconPath: '/images/location.png'
+      iconPath: '/pages/images/location.png'
     }]
   },
   onReady: function (e) {
@@ -48,7 +48,7 @@ Page({
   },
   includePoints: function () {
     this.mapCtx.includePoints({
-      padding: [10],
+      padding: [100],
       points: [{
         latitude: 23.10229,
         longitude: 113.3345211,
